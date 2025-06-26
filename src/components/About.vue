e788215456d0fbc8c3aa8bbfa69ad309c4e65f0d
Prarim Idrizi <pi61701@ubt-uni.net>
2025-06-26 16:53:36 +0200

    About, Contact

---
+++ src/components/About.vue
@@ -0,0 +1,88 @@
+<template>
+<div>
+    <div class="bg-prima1 py-5 mb-5">
+     <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <div class="col-lg-12">
+             <h1 class="display-4 text-white mt-5 mb-2" style="text-shadow: 3px 3px 5px #000000; font-size: 120px;">About us</h1>
+          </div>
+        </div>
+      </div>
+     </div>
+     <div class="container">
+       <div class="separator">About us</div><br>
+          <div class="row space2">
+              <!-- <div class="col-lg-8 col-sm-6 portfolio-item space2"> -->
+                <div class="card">
+                
+                  <div class="card-body">
+                    <h4 class="card-title">
+                      <br>
+                    </h4>
+                    <div class="about text-center"> <p class="aboutmep">
+                      Mirë se vini në faqen tonë të internetit të shitësve të Comictorium! Ne jemi 
+                      të apasionuar pas librave komikë dhe misioni ynë është t'u ofrojmë 
+                      koleksionistëve dhe fansave një përzgjedhje të larmishme dhe cilësore të 
+                      librave komikë nga të gjitha zhanret dhe botuesit.
+
+                      <br><br>
+                      Ekipi ynë përbëhet nga lexues dhe koleksionistë të etur të librave komikë, 
+                      të cilët e kuptojnë vlerën dhe rëndësinë e këtyre veprave të artit. Ne 
+                      përpiqemi të krijojmë një përvojë blerjeje të përshtatshme dhe të 
+                      këndshme për të gjithë klientët tanë, pavarësisht nëse jeni një koleksionist 
+                      me përvojë ose sapo po filloni.
+
+                      <br><br>
+                      Ne jemi të përkushtuar të ofrojmë shërbim të shkëlqyer ndaj klientit dhe 
+                      të sigurojmë që çdo porosi të përpunohet dhe dërgohet menjëherë. Ne 
+                      ofrojmë një gamë të gjerë librash komik, duke përfshirë botime të vjetra 
+                      dhe të rralla, si dhe botimet më të fundit nga botuesit tuaj të preferuar.
+
+                      <br><br>
+                      Faqja jonë e internetit është krijuar për të qenë një dyqan i vetëm për të 
+                      gjitha nevojat tuaja për librat komik. Ne ofrojmë porosi të sigurt në 
+                      internet dhe një funksion të përshtatshëm kërkimi për t'ju ndihmuar të 
+                      gjeni saktësisht atë që kërkoni. Ne gjithashtu ofrojmë informacione të 
+                      detajuara dhe imazhe të çdo libri komik për t'ju ndihmuar të merrni një 
+                      vendim të informuar përpara se të bëni një blerje.
+
+                      <br><br>
+                      Në faqen tonë të internetit të shitësve të Comictorium, ne po 
+                      përditësojmë vazhdimisht inventarin tonë me tituj të rinj dhe emocionues, 
+                      prandaj sigurohuni që të kontrolloni shpesh për të parë se çfarë ka të re. 
+                      Faleminderit që na zgjodhët si burimin tuaj të preferuar për librat komik 
+                      dhe ne mezi presim t'ju shërbejmë!
+                      </p><br>
+
+                      <!-- social media -->
+                      <div class="row" style="height: 100px; justify-content: space-evenly;">
+                        <a href="https://www.instagram.com"><img class="socialmedia" src="https://cdn.discordapp.com/attachments/956671700936839259/1084500117173846058/1161953_instagram_icon.png"></a>
+                        <a href="https://www.viber.com/en/"><img class="socialmedia" src="https://cdn.discordapp.com/attachments/956671700936839259/1084500117412917359/64-viber-256.webp"></a>
+                        <a href="https://www.facebook.com"><img class="socialmedia" src="https://cdn.discordapp.com/attachments/956671700936839259/1084500117844918292/5305154_fb_facebook_facebook_logo_icon.png"></a>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              <!-- </div> -->
+              
+        
+            <!-- <div class="col-lg-4 portfolio-item ">
+              <br><br><br>
+              <div class="card bg-transparent border-0">
+                    <img src="https://cdn.discordapp.com/attachments/956671700936839259/1084499612632629309/image.png" class="fotoabout" style="background-size: cover; background-position: center;">
+                    <br><br>
+                  <img src="https://i.pinimg.com/originals/bd/da/47/bdda47151b710aec794e3264405c3863.jpg" class="fotoabout" >
+                  </div>
+            </div> -->
+        </div>
+      </div>
+ </div>
+ 
+</template>
+ <style>
+
+ .dark .separator{
+   color: white
+ }
+ </style>
