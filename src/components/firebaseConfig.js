c90843aad4b94cc3f53d47267b5bda4b1418a71d
Fisnik Hyseni <fh51748@ubt-uni.net>
2025-06-30 19:34:40 +0200

    toggle

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAOiofyNQCcuTgOMZlCk_QyLWTfdqfLx6w",
+  authDomain: "comicbooks-68a60.firebaseapp.com",
+  projectId: "comicbooks-68a60",
+  storageBucket: "comicbooks-68a60.appspot.com",
+  messagingSenderId: "375380011159",
+  appId: "1:375380011159:web:666c469c60f33b10b8e1d2"
+  };
+  
